b71fe8f99e9a9807eb2ff328cf653fd22b50514d
Max Nicolai <maxvnicolai@gmail.com>
2020-09-05 16:36:06 -0700

    added more test  data to seed file

--- db/employees_seed.sql
+++ db/employees_seed.sql
@@ -1,17 +1,21 @@
 USE employee_db;
+
 -- Test department data
 INSERT INTO department (name)
 VALUES ('Marketing');
+
 -- Test role data
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 60000, 1);
+VALUES ('Manager', 60000, 1),('Associate', 40000, 1),('Intern', 0, 1);
+
 -- Test employee data
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ('John', 'Doe', 1);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ('John', 'Doe', 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jack', 'Doe', 3, 1), ('Jane', 'Doe', 2, 1);
 
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
+
