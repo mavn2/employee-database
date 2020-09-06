e6f5ed2b5bd1ee4aae7735e0e016b502ca16c282
Max Nicolai <maxvnicolai@gmail.com>
2020-09-06 16:57:32 -0700

    Changed the table names to plurals

--- db/employee_db.sql
+++ db/employee_db.sql
@@ -5,30 +5,30 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- Creates Department table
-CREATE TABLE department (
+CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 -- Create Role table
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL, 
   department_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 -- Creates Employee table
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT,
   PRIMARY KEY(id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- db/employees_seed.sql
+++ db/employees_seed.sql
@@ -1,17 +1,17 @@
 USE employee_db;
 
 -- Test department data
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES ('Marketing'),('Development');
 
 -- Test role data
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ('Manager', 60000, 1),('Associate', 40000, 1),('INTERN', 0, 1), ('Lead', 60000, 2), ('Engineer', 45000, 2);
 
 -- Test employee data
-INSERT INTO employee (first_name, last_name, role_id)
+INSERT INTO employees (first_name, last_name, role_id)
 VALUES ('John', 'Doe', 1), ('Susan', 'Susan', 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES ('Jack', 'Doe', 3, 1), ('Jane', 'Doe', 2, 1), ('Bill', 'Hicock', 5, 2), ('John', 'Wayne', 3, 2);
 
