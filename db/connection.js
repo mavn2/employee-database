a397ce89533faf9913348da248e7affade0ffc96
Max Nicolai <maxvnicolai@gmail.com>
2020-09-06 16:33:04 -0700

    pushing connection file

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,26 @@
 //Set up db connection
+
+//Retrieves password exported from local password.js file
+const password = require('./password')
+
+//Required NPM module
+const mysql = require('mysql');
+
+//Creates connection 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: password,
+  database: 'employee_db'
+});
+
+//Connection test code executes if this file is run
+connection.connect(err => {
+  if (err){
+    console.error(`Error connecting: ${err.stack}`);
+    return;
+  };
+  console.log(`Connected on ${connection.threadId}`);
+});
+
+module.exports = connection
