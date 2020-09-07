d857b7af17a9cb3695b7a21c68f1d91afb5a7759
Max Nicolai <maxvnicolai@gmail.com>
2020-09-06 19:56:28 -0700

    began writing prompts array/objects for inquirer, first basic choices working

--- prompts.js
+++ prompts.js
@@ -1,3 +1,24 @@
-//Inquirer prompts written here
-//Required module
-const inquirer = require('inquirer');
+//Exports prompts as single object:
+module.exports = {
+  Main: [
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: [
+        {
+        name: 'View All Employees',
+        value: 'v_employees'
+        },
+        {
+        name: 'View All Roles',
+        value: 'v_roles'
+        },
+        {
+        name: 'View All Departments',
+        value: 'v_departments'
+        }
+      ]
+    }
+  ],
+}
