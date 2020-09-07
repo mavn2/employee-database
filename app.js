d0a26434eb70f106d24d4d7a8f14497fdaadfe2f
Max Nicolai <maxvnicolai@gmail.com>
2020-09-06 19:57:36 -0700

    fns to display prompts/data working

--- app.js
+++ app.js
@@ -1,4 +1,60 @@
-//Required modules
-require ('console.table')
+'use strict';
 
-//fn to run cli on launch
+//Required modules
+const inquirer = require('inquirer');
+
+//Required files/folders
+const db = require('./db');
+const prompts = require('./prompts');
+//require ('console.table');
+
+//Runs cli on launch
+mainPrompt();
+
+//Calls 
+async function mainPrompt() {
+  const {choice} = await inquirer.prompt(prompts.Main)
+  switch (choice){
+   case 'v_employees':
+      viewAllEmployees();
+      break;
+    case 'v_roles':
+      viewAllRoles();
+      break;
+    case 'v_departments':
+      viewAllDepartments()
+  };
+};
+
+//Searches db, then displays all employees
+async function viewAllEmployees() {
+  const employees = await db.viewEmployees();
+
+  console.log('\n')
+  console.table(employees);
+
+  mainPrompt();
+};
+
+//Searches db, then displays all roles
+async function viewAllRoles() {
+  const roles = await db.viewRoles();
+
+  console.log('\n');
+  console.table(roles);
+
+  mainPrompt();
+};
+
+//Searches db, then displays all roles
+async function viewAllDepartments() {
+  const deps = await db.viewDepartments();
+
+  console.log('\n');
+  console.table(deps);
+
+  mainPrompt();
+};
+
+
+
